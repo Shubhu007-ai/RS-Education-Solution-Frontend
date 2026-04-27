629166920d0b8b2430b4f07bd4560cf0d639c161
Shubham Patel <snohara340@gmail.com>
2026-04-27 16:27:25 +0530

    remove warning for unused variable "err" in catch block

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -68,6 +68,7 @@ export default function Contact() {
         message: "",
       });
     } catch (err) {
+      console.error(err);
       toast.error("Something went wrong ❌");
     } finally {
       setLoading(false);
